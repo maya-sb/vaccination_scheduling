b57232ef5c878ac425a2dca0d81d066ceec8ccd9
Mayara Barbosa <mayarabarbosa2206@gmail.com>
2021-05-27 16:27:05 -0300

    feat: Create model VaccinationCenter and ServiceGroup

--- vaccination/models.py
+++ vaccination/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class VaccinationCenter(models.Model):
+    cnes = models.IntegerField(primary_key=True, verbose_name='CNES')
+    name = models.CharField(max_length=200, verbose_name='Nome')
+    address = models.CharField(max_length=200, verbose_name='Logradouro')
+    neighborhood = models.CharField(max_length=200, verbose_name='Bairro')
+    city = models.CharField(max_length=200, verbose_name='Cidade')
+
+    class Meta:
+        verbose_name = 'Local de Vacinação'
+        verbose_name_plural = 'Locais de Vacinação'
+        ordering = ['name', 'city']
+
+
+class ServiceGroup(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Nome")
+    min_age = models.IntegerField(verbose_name="Idade mínima")
+
+    class Meta:
+        verbose_name = 'Grupo de Atendimento'
+        verbose_name_plural = 'Grupos de Atendimento'
+        ordering = ['-min_age']
--- vaccination_scheduling/settings.py
+++ vaccination_scheduling/settings.py
@@ -9,16 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -60,10 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vaccination_scheduling.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -97,10 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Maceio'
@@ -110,7 +98,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/

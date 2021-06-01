74be32da5c7eb8b72760fb11d6984c5b7478f200
Mayara Barbosa <mayarabarbosa2206@gmail.com>
2021-05-31 22:47:52 -0300

    feat: Create Vaccine, Scheduling, SchedulingCitizen, Room and RoomScheduling models

--- vaccination/models.py
+++ vaccination/models.py
@@ -48,3 +48,71 @@ class Citizen(models.Model):
 
     def __str__(self):
         return self.user.name
+
+
+class Vaccine(models.Model):
+    name = models.CharField(verbose_name='Nome', max_length=200)
+    manufacturer = models.CharField(verbose_name='Fabricante', max_length=200)
+
+    class Meta:
+        verbose_name = 'Vacina'
+        verbose_name_plural = 'Vacinas'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Scheduling(models.Model):
+    date = models.DateField(verbose_name='Data')
+    slot = models.TimeField(verbose_name='Horário')
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Agendamento'
+        verbose_name_plural = 'Agendamentos'
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'{self.vaccine.name} - {self.date} - {self.slot}'
+
+
+class SchedulingCitizen(models.Model):
+    STATUS_CHOICES = (
+        ('a', 'Agendado'),
+        ('v', 'Vacinado'),
+        ('c', 'Cancelado'),
+    )
+    status = models.CharField(choices=STATUS_CHOICES, max_length=200)
+    scheduling = models.ForeignKey(Scheduling, verbose_name='Agendamento', on_delete=models.CASCADE)
+    citizen = models.OneToOneField(Citizen, verbose_name='Cidadão', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'AgendamentoCidadão'
+        verbose_name_plural = 'AgendamentosCidadãos'
+
+
+class Room(models.Model):
+    name = models.CharField(verbose_name='Sala', max_length=200)
+    center = models.ForeignKey(VaccinationCenter, verbose_name='Local', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Sala'
+        verbose_name_plural = 'Salas'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name} - {self.center.name}'
+
+
+class RoomScheduling(models.Model):
+    num_vacancies = models.IntegerField(verbose_name='Vagas')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    scheduling = models.ForeignKey(Scheduling, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'SalaAgendamento'
+        verbose_name_plural = 'SalaAgendamentos'
+
+    def change_num_vacancies(self, vacancies):
+        self.num_vacancies += vacancies
